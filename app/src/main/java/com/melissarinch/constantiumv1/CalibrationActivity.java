aed994d41a06a67f60a590ea012b050db9db6669
Laura Ing <lauraeing@gmail.com>
2020-01-30 20:08:49 -0500

    beginnings of calibration activity

---
+++ app/src/main/java/com/melissarinch/constantiumv1/CalibrationActivity.java
@@ -0,0 +1,14 @@
+package com.melissarinch.constantiumv1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalibrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calibration);
+    }
+}
